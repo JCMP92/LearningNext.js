e65db12430844c153198cefda012d9a649548f52
Juan C. Montes Pesquera <arq_juanmp@outlook.com>
2023-04-17 16:01:16 -0600

    Creating pages

---
+++ learning-next/pages/hello.js
@@ -0,0 +1,15 @@
+export default function Hello() {
+  return (
+    <>
+      <h1>Learn NextJS</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut urna purus,
+        venenatis id rutrum eget, placerat ut ligula. In vitae purus gravida
+        sapien dictum vestibulum. Nulla facilisi. Curabitur semper metus non sem
+        ultricies, vitae blandit dui accumsan. Proin pharetra neque libero, eget
+        posuere arcu tincidunt et. Nulla ac vehicula massa. Integer non dapibus
+        nunc. Nullam non nunc at lorem tristique lobortis.
+      </p>
+    </>
+  );
+}
---
+++ learning-next/pages/learn/next.js
@@ -0,0 +1,23 @@
+export default function LearnNextJS() {
+  return (
+    <>
+      <h1>Routed Learn NextJS</h1>
+      <p>
+        In sit amet quam sagittis, convallis sem non, dignissim magna. Fusce
+        porta dui suscipit faucibus accumsan. Quisque porttitor urna diam, ac
+        semper lorem convallis vel. Quisque nec dapibus neque. Sed varius augue
+        nisl, in consequat dolor ornare lobortis. Vestibulum vel lorem ac risus
+        fermentum volutpat. Morbi quis tincidunt leo, tincidunt ornare justo. Ut
+        pulvinar ante magna. Quisque porttitor convallis eleifend. Orci varius
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+        mus. Vestibulum dictum ligula mi, in egestas risus gravida sit amet. In
+        hac habitasse platea dictumst. Vestibulum ut felis ut ex egestas mollis
+        id ac ex. Nunc iaculis, diam non venenatis bibendum, augue dolor sodales
+        purus, vitae fringilla augue mauris ac erat. Fusce ac nibh ligula.
+        Suspendisse scelerisque leo erat, vitae volutpat orci molestie sit amet.
+        Suspendisse volutpat odio sem, a tincidunt lectus volutpat vitae.
+        Aliquam at tincidunt sapien, a congue neque.
+      </p>
+    </>
+  );
+}
